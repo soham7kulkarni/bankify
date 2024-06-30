d57dbb88dfb53ba93bc079c69e740b9c4151a3bc
Soham Kulkarni <soham7kulkarni@gmail.com>
2024-06-29 18:46:48 -0700

    Complete Auth page UI

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,14 +20,18 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 const AuthForm = ( { type }: {type:string}) => {
+    const router = useRouter();
     const [user, setuser] = useState(null)
     const [isLoading, setIsLoading] = useState(false);
 
+    const formSchema = authFormSchema(type);
+
       // 1. Define your form.
-  const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: ""
@@ -35,13 +39,29 @@ const AuthForm = ( { type }: {type:string}) => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    
     setIsLoading(true);
-    console.log(isLoading);
-    console.log(values);
-    setIsLoading(false);
+    try {
+        // Sign up with Appwrite & create plaid token
+        
+        if ( type === 'sign-up') {
+            // const userData = await SignUp(data);
+            // setuser(newUser);
+        }
+        if ( type === 'sign-in') {
+            // const response = await SignIn({
+            //     email: data.email,
+            //     password: data.password,
+            // })
+
+            // if (response) { router.push('/')}
+        }
+    } catch (error) {
+        console.log(error);
+    } finally {
+        setIsLoading(false);
+    }
   }
 
   return (
@@ -82,7 +102,25 @@ const AuthForm = ( { type }: {type:string}) => {
                 <>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        { type === 'sign-up' && (
+                            <>
+                                <div className='flex gap-4'>
+                                <CustomInput control = { form.control } name = 'firstName' label = 'First Name' placeholder= 'Enter Your first name'  />
+                                <CustomInput control = { form.control } name = 'lastName' label = 'Last Name' placeholder= 'Enter Your last name'  />
+                                </div>
+                                <CustomInput control = { form.control } name = 'address1' label = 'Address' placeholder= 'Enter Your specific address'  />
+                                <CustomInput control = { form.control } name = 'city' label = 'City' placeholder= 'Enter Your city'  />
 
+                                <div className='flex gap-4'>
+                                <CustomInput control = { form.control } name = 'state' label = 'State' placeholder= 'Example: CA'  />
+                                <CustomInput control = { form.control } name = 'postalCode' label = 'Postal Code' placeholder= 'Example: 11101'  />
+                                </div>
+                                <div className='flex gap-4'>
+                                <CustomInput control = { form.control } name = 'dateOfBirth' label = 'Date of Birth' placeholder= 'YYYY-MM-DD'  />
+                                <CustomInput control = { form.control } name = 'ssn' label = 'SSN' placeholder= 'Example: 12345'  />
+                                </div>
+                            </>
+                        )}
                         <CustomInput control = { form.control } name = 'email' label = 'Email' placeholder= 'Enter Your Email'  />
                         <CustomInput control = { form.control } name = 'password' label = 'Password' placeholder= 'Enter Your Password'  />
                         <div className='flex flex-col gap-4'>
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+'use server';
+
+export const signIn = async () => {
+    try {
+        
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
+
+export const signUp = async () => {
+    try {
+        
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
