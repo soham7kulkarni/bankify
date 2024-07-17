4369de65ad31bd10b82dae10e3bb16de28824a8f
Soham Kulkarni <soham7kulkarni@gmail.com>
2024-07-16 19:39:19 -0700

    Implement Boiler plate code to link Plaid API with app

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,6 +22,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ( { type }: {type:string}) => {
     const router = useRouter();
@@ -98,9 +99,9 @@ const AuthForm = ( { type }: {type:string}) => {
             </header>
             { user ? 
             ( <div className='flex flex-col gap-4'>
-
-            </div>)
-            : (
+                    <PlaidLink user = {user} variant="primary" />
+            </div>
+            ): (
                 <>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
