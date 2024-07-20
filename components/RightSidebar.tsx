aea50809df4c435bfde4fe70d301d1d458eb5303
Soham Kulkarni <soham7kulkarni@gmail.com>
2024-07-19 17:51:07 -0700

    Execute Recent Transactions

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -11,11 +11,11 @@ const RightSidebar = ( { user, transactions, banks}: RightSidebarProps )=> {
             <div className='profile-banner' />
             <div className='profile'>
                 <div className='profile-img'>
-                    <span className='text-5xl font-bold text-blue-500'> {user.name[0]}</span>
+                    <span className='text-5xl font-bold text-blue-500'> {user.firstName[0]}</span>
                 </div>
                 <div className='profile-details'>
                     <h1 className='profile-name'>
-                        {user.name}
+                        {user.firstName} {user.lastName}
                     </h1>
                     <p className='profile-email'>
                          {user.email}
@@ -40,7 +40,7 @@ const RightSidebar = ( { user, transactions, banks}: RightSidebarProps )=> {
                         <BankCard
                         key={banks[0].$id}
                         account={banks[0]}
-                        userName={user.name}
+                        userName={`${user.firstName} ${user.lastName}`}
                         showBalance = {false} />
                     </div>
                     {banks[1] && (
@@ -48,7 +48,7 @@ const RightSidebar = ( { user, transactions, banks}: RightSidebarProps )=> {
                             <BankCard
                             key={banks[1].$id}
                             account={banks[1]}
-                            userName={user.name}
+                            userName={`${user.firstName} ${user.lastName}`}
                             showBalance = {false} />
                         </div>
 
